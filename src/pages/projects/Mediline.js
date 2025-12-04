09c5ff3f9781279200782bec3d6ca084de978994
poojasheoran12 <sheoranpooja726@gmail.com>
2025-12-04 19:47:45 +0530

    Added routing and new project pages

---
+++ src/pages/projects/Mediline.js
@@ -0,0 +1,10 @@
+export default function Mediline() {
+  return (
+    <div className="p-10">
+      <h1 className="text-3xl font-bold">MediLine Project</h1>
+      <p className="mt-4 opacity-80">
+        Detailed description of the MediLine project will go here.
+      </p>
+    </div>
+  );
+}
